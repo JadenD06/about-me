d8120374519febe06baf40726ae2023f0592c50a
JadenD06 <contact.jadend@gmail.com>
2026-01-01 22:26:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-fetch("song.json")
-  .then(response => response.json())
-  .then(data => {
-    document.getElementById("date").textContent = data.date;
-    document.getElementById("title").textContent = data.title;
-    document.getElementById("artist").textContent = data.artist;
-    document.getElementById("note").textContent = data.note;
-  })
-  .catch(error => {
-    console.error("Error loading song data:", error);
-  });
+    fetch("song.json")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("date").textContent = data.date;
+        document.getElementById("title").textContent = data.title;
+        document.getElementById("artist").textContent = data.artist;
+        document.getElementById("note").textContent = data.note;
+    })
+    .catch(error => {
+        console.error("Error loading song data:", error);
+    });
+
+    const dateElement = document.getElementById("date");
+
+    const today = new Date();
+
+    const options = {
+    year: "numeric",
+    month: "long",
+    day: "numeric"
+    };
+
+    dateElement.textContent = today.toLocaleDateString("en-US", options); 
